846809586d5f206e88fb2cf81867b4fb5d074b81
YangSpring114 <blessing_114514@qq.com>
2024-04-16 18:59:13 +0800

    添加基准测试项目

---
+++ MinecraftLaunch.BanchTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using MinecraftLaunch.Components.Checker;
+using MinecraftLaunch.Components.Resolver;
+
+var summary = BenchmarkRunner.Run<BenchmarkClass>();
+
+[MemoryDiagnoser]
+public class BenchmarkClass {
+    private readonly ResourceChecker _checker;
+    private readonly GameResolver _gameResolver = new("D:\\GamePackages\\.minecraft");
+
+    public BenchmarkClass() {
+        _checker = new(_gameResolver.GetGameEntity("Life in the village"));
+    }
+
+    //[Benchmark]
+    //public Task<bool> RunTask() {
+    //    return _checker.CheckAsync1();
+    //}
+
+    [Benchmark]
+    public ValueTask<bool> RunValueTask() {
+        return _checker.CheckAsync();
+    }
+}
